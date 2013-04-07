42a6e626ad808228d844c931c57a406702a26d2b
James Booth <boothj5@gmail.com>
2013-04-07 01:15:07 +0100

    Tided up html, add link tracker

---
+++ js/trackclicks.js
@@ -0,0 +1,47 @@
+var track = function (category, label) {
+//    alert("Clicked " + category + ", " + label);
+   _gaq.push(['_trackEvent', category, "clicked", label]);
+};
+
+$(document).ready(function () {
+    var profanity_links = "#link-profanity, #header-link-profanity, #redirect-profanity",
+        head_unit_links = "#link-head-unit, #header-link-head-unit",
+        jarch_links = "#link-jarch, #header-link-jarch",
+        pyloc_links = "#link-pyloc, #header-link-pyloc",
+        cv_links = "#link-cv, #header-link-cv";
+
+    $(cv_links).click(function (e) {
+        track("professional", "cv");
+    });
+
+    $(profanity_links).click(function (e) {
+        track("projects", "profanity");
+    });
+    
+    $(head_unit_links).click(function (e) {
+        track("projects", "head-unit");
+    });
+    
+    $(jarch_links).click(function (e) {
+        track("projects", "jarch");
+    });
+    
+    $(pyloc_links).click(function (e) {
+        track("projects", "pyloc");
+    });
+
+    $("#header-link-learning").click(function (e) {
+        track("links", "learning");
+    });
+    
+    $("[id^=find]").click(function (e) {
+        var site = $(this).attr("id").split("-")[1];
+        track("find", site);
+    });
+    
+    $("[id^=github]").click(function (e) {
+        var repo = $(this).attr("id").split("-")[1];
+        track("github-repo", repo);
+    });
+
+});
